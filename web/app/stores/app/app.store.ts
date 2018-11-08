0f05891674803b2e9b1b60db77e40f69ff26bc43
Orit Persik <oritp@signature-it.com>
2018-11-08 13:05:49 +0200

    upadte app store

--- web/app/stores/app/app.store.ts
+++ web/app/stores/app/app.store.ts
@@ -2,21 +2,24 @@ import { NgModule } from "@angular/core";
 import { StoreModule } from "@ngrx/store";
 import { StoreDevtoolsModule } from "@ngrx/store-devtools";
 import { catalogueReducer, CatalogueStoreModule } from "@signature-it/ngx-catalogue";
+import { cartReducerGeneric, CartStoreModule} from '@signature-it/ngx-generic';
 import { appReducer } from "./reducers/app.reducer";
 
 @NgModule({
     imports: [
         StoreModule.provideStore({
             app: appReducer,
-            catalogue: catalogueReducer
+            catalogue: catalogueReducer,
+            cartState: cartReducerGeneric
         }),
         CatalogueStoreModule,
+        CartStoreModule,
         StoreDevtoolsModule.instrumentOnlyWithExtension({
             maxAge: 25
         })
     ],
     providers: [
-
+        CartStoreModule
     ]
 })
 export class AppStoreModule { }
