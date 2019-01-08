4e6f7e980f973529f8f183ea37972fb1337b552b
Orit Persik <oritp@signature-it.com>
2019-01-08 13:12:12 +0200

    angular 7

--- web/app/app.component.ts
+++ web/app/app.component.ts
@@ -1,12 +1,23 @@
-import { Component, ViewContainerRef } from '@angular/core';
-import {
-    AppComponent as AppComponent_Generic
-} from "@signature-it/ngx-generic";
+// import { Component, ViewContainerRef } from '@angular/core';
+// import {
+//     AppComponent as AppComponent_Generic
+// } from "@signature-it/ngx-generic";
+//
+// @Component({
+// 	selector: 'app-root',
+// 	templateUrl: './app.component.html'
+// })
+// export class AppComponent extends AppComponent_Generic {
+//
+// }
+
+import { Component } from '@angular/core';
 
 @Component({
 	selector: 'app-root',
-	templateUrl: './app.component.html'
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
-export class AppComponent extends AppComponent_Generic {
-
+export class AppComponent {
+	title = 'app-generic-new';
 }
--- web/app/app.routes.ts
+++ web/app/app.routes.ts
@@ -1,29 +1,41 @@
-import { RouterModule } from '@angular/router';
-import { NgModule } from "@angular/core";
-import { TranslateModule } from "@ngx-translate/core";
-import { AppCustomPreloader } from "@signature-it/ngx-generic";
+// import { RouterModule } from '@angular/router';
+// import { NgModule } from "@angular/core";
+// import { TranslateModule } from "@ngx-translate/core";
+// import { AppCustomPreloader } from "@signature-it/ngx-generic";
+//
+// @NgModule({
+//     imports: [
+//         RouterModule.forRoot([
+//                 {
+//                     path: '',
+//                     loadChildren: './modules/layout.module#MainLayoutModule'
+//                 },
+//                 {
+//                     path: '**',
+//                     redirectTo: '404'
+//                 }
+//             ],
+//             {preloadingStrategy: AppCustomPreloader}
+//         ),
+//         TranslateModule
+//     ],
+//     exports: [RouterModule],
+//     providers: [AppCustomPreloader]
+// })
+// /**
+//  * @extends AppRoutingModuleGeneric
+//  */
+// export class AppRoutingModule {}
+
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [];
 
 @NgModule({
-    imports: [
-        RouterModule.forRoot([
-                {
-                    path: '',
-                    loadChildren: './modules/layout.module#MainLayoutModule'
-                },
-                {
-                    path: '**',
-                    redirectTo: '404'
-                }
-            ],
-            {preloadingStrategy: AppCustomPreloader}
-        ),
-        TranslateModule
-    ],
-    exports: [RouterModule],
-    providers: [AppCustomPreloader]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-/**
- * @extends AppRoutingModuleGeneric
- */
-export class AppRoutingModule {}
+export class AppRoutingModule { }
+
 
