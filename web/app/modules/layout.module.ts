d6202fe4565490fad2c630ab75e8d7c332f357be
avital <avital@signature.com>
2019-06-03 14:03:17 +0300

    SIG-8082 Ang7 - Product Overview page

--- web/app/modules/layout.module.ts
+++ web/app/modules/layout.module.ts
@@ -5,13 +5,12 @@ import {PageTitleModuleGeneric} from '../../../projects/signature-it/ngx-generic
 import {MainFooterModuleGeneric} from '../../../projects/signature-it/ngx-generic/src/lib/components/layouts/main/footer/main-footer.module';
 import {MainFooterComponent} from '../../../projects/signature-it/ngx-generic/src/lib/components/layouts/main/footer/main-footer';
 import {RouterModule} from '@angular/router';
-import {BreadcrumbsModule} from '../../../projects/signature-it/ngx-generic/src/lib/components/breadcrumbs/breadcrumbs.module';
-import {ContentloaderModule} from '../../../projects/signature-it/ngx-generic/src/lib/components/contentloader/contentloader.module';
-import {LoginService} from '../../../projects/signature-it/ngx-generic/src/lib/services/login';
-import {RequestOptionsProvider} from '../../../projects/signature-it/ngx-generic/src/lib/services/default-request-options';
-import {CmsModule} from '../../../projects/signature-it/ngx-generic/src/lib/components/cms/cms.module';
+import {BreadcrumbsModule,
+    ContentloaderModule,
+    RequestOptionsProvider,
+    CmsModule,
+    MenuNavigationService} from '@signature-it/ngx-generic';
 import {MatSidenavModule} from '@angular/material';
-import {MenuNavigationService} from '../../../projects/signature-it/ngx-generic/src/lib/services/menu-navigation';
 import {CommonModule} from '@angular/common';
 import {TranslateModule} from '@ngx-translate/core';
 import {MainLayoutComponent} from './main/main';
