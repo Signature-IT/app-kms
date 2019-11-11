d651e83c526b7c1c413b91ec0f250593c8d172b4
Orit Persik <oritp@signature-it.com>
2019-11-11 09:43:01 +0200

    update account routes

---
+++ web/app/modules/account/approve-orders.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { ApproveOrdersGenericModule } from '@signature-it/ngx-catalogue';
+
+@NgModule({
+    imports: [
+        ApproveOrdersGenericModule
+    ]
+})
+/**
+ * @extends ApproveOrdersGenericModule
+ */
+export class ApproveOrdersModule {}
