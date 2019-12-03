531b6c9964fae1479146800d376fb9d9627f0bd0
naomi <naomi@signature-it.com>
2019-12-03 12:06:35 +0200

    SIG-8337 Mobile header SEARCH and USER dropdowns should change

--- web/app/components/layout/main/header/search-form/search-form.ts
+++ web/app/components/layout/main/header/search-form/search-form.ts
@@ -3,7 +3,7 @@ import { MainHeaderSearchFormComponentGeneric} from '@signature-it/ngx-generic';
 
 @Component({
     selector: 'main-header-search-form',
-    templateUrl: './search-form.html',
-    styleUrls: ['./search-form.scss'],
+    templateUrl: '../../../../../../../projects/signature-it/ngx-generic/src/lib/components/layouts/main/header/search-form/search-form.html',
+    styleUrls: ['../../../../../../../projects/signature-it/ngx-generic/src/lib/components/layouts/main/header/search-form/search-form.scss'],
 })
 export class MainHeaderSearchFormComponent extends MainHeaderSearchFormComponentGeneric {}
