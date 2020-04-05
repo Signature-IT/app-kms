ddf349aad02b3135329e312dcceadf708550e099
Orit Persik <oritp@signature-it.com>
2020-04-05 08:43:20 +0300

    languages in mobile

--- web/app/components/layout/main/header/navbar-mobile/side/navbar-mobile-side.ts
+++ web/app/components/layout/main/header/navbar-mobile/side/navbar-mobile-side.ts
@@ -1,16 +1,33 @@
-import { Component, ViewEncapsulation } from '@angular/core';
-import { MainHeaderNavbarMobileSideComponentGeneric } from '@signature-it/ngx-generic';
-
+import {
+    Component,
+    ViewEncapsulation,
+    Input,
+    Output,
+    EventEmitter,
+    ViewChild,
+    OnInit
+} from '@angular/core';
+import { MobileLinksDTO, MainHeaderNavbarMobileSideComponentGeneric, LanguageService } from '@signature-it/ngx-generic';
 
 @Component({
     selector: 'main-header-navbar-mobile-side',
     templateUrl: './navbar-mobile-side.html',
     styleUrls: ['./navbar-mobile-side.scss'],
-    encapsulation: ViewEncapsulation.None,
-    inputs: [
-        'data',
-        'is-sidebar-open'
-    ],
-    outputs: ['toggle']
+    encapsulation: ViewEncapsulation.None
 })
-export class MainHeaderNavbarMobileSideComponent extends MainHeaderNavbarMobileSideComponentGeneric {}
+export class MainHeaderNavbarMobileSideComponent extends MainHeaderNavbarMobileSideComponentGeneric implements OnInit {
+
+    @ViewChild('sidenav', {static: true}) sidenav: any;
+    @Input() data: MobileLinksDTO[];
+    @Output() toggle = new EventEmitter();
+
+    constructor(langSvc: LanguageService) {
+        super(langSvc);
+    }
+
+
+    ngOnInit() {
+        super.ngOnInit();
+    }
+
+}
